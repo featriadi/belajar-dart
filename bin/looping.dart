96a0d0a84c16c85aa61ef13232c060bb427759ce
featriadi <aditya.fitriadi99@gmail.com>
2021-03-17 17:04:53 +0700

    Looping

---
+++ bin/looping.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+main(List<String> arguments) {
+  //Looping For
+  // for(int counter = 0; counter < 5; counter += 1){
+  //   print("Hello World!");
+  // }
+
+  //Looping While
+  // int i = 0;
+  // while(i < 5){
+  //   print("Ini angka " + i.toString());
+  //   i += 1;
+  // }
+
+  //Looping Do While
+  int i = 0;
+  do {
+    print("Ini angka " + i.toString());
+    i += 1;
+  } while(i < 5);
+}
